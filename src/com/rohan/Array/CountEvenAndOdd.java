45065b2fbf6286a4675dd16fbc42cfb3bcf092ea
Rohan <rohanprasad1434@gmail.com>
2025-10-05 21:08:32 +0530

    crack

---
+++ src/com/rohan/Array/CountEvenAndOdd.java
@@ -0,0 +1,18 @@
+package com.rohan.Array;
+// Time Complexity = O(n)
+// Space Complexity = O(1)
+public class CountEvenAndOdd {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7};
+        int even = 0;
+        int odd = 0;
+        for (int num:arr) {
+            if(num%2==0){
+                even++;
+            }else {
+                odd++;
+            }
+        }
+        System.out.println("Even "+even+" Odd "+odd);
+    }
+}
