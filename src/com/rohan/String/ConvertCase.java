da177d52f02be39b4828fb7e98b4c99aa07f93f5
Rohan <rohanprasad1434@gmail.com>
2025-10-05 21:07:51 +0530

    crack

---
+++ src/com/rohan/String/ConvertCase.java
@@ -0,0 +1,9 @@
+package com.rohan.String;
+
+public class ConvertCase {
+    public static void main(String[] args) {
+        String name = "Rohan";
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
+    }
+}
