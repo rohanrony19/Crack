def199afcd3b7321e9040daf9e1533c5abf53cea
Rohan <rohanprasad1434@gmail.com>
2025-10-06 22:19:48 +0530

    crack

---
+++ src/com/rohan/Array/LargestElement.java
@@ -0,0 +1,15 @@
+package com.rohan.Array;
+// Time Complexity = O(n)
+// Space Complexity = O(1)
+public class LargestElement {
+    public static void main(String[] args) {
+        int[] arr = {23,56,78,19,48};
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if(max < arr[i]){
+                max = arr[i];
+        }
+        }
+        System.out.println("Maximum Element " + max);
+    }
+}
