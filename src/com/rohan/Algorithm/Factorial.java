e104d1f5d260b81df878884aae5dee492312b1a4
Rohan <rohanprasad1434@gmail.com>
2025-10-06 22:19:16 +0530

    crack

---
+++ src/com/rohan/Algorithm/Factorial.java
@@ -0,0 +1,20 @@
+package com.rohan.Algorithm;
+// 5! = 5*4*3*2*1
+
+// Time Complexity = O(n)
+// Space Complexity = O(1)
+
+public class Factorial {
+    static int Fact(int n) {
+        int result = 1;
+        for (int i = 2; i <= n; i++) {
+            result = result * i;
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println("Factorial of "+ n + " is " + Fact(n));
+    }
+}
+
