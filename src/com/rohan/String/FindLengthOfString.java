41bc7349646540f0f38ab69f23970e25127fc3a8
Rohan <rohanprasad1434@gmail.com>
2025-10-06 22:19:29 +0530

    crack

---
+++ src/com/rohan/String/FindLengthOfString.java
@@ -0,0 +1,8 @@
+package com.rohan.String;
+
+public class FindLengthOfString {
+    public static void main(String[] args) {
+        String name = "Rohan";
+        System.out.println("Length of "+ name + " is " + name.length());
+    }
+}
