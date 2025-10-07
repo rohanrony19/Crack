ab1047717e51e7080e13fc80625aa62aaee2dfea
Rohan <rohanprasad1434@gmail.com>
2025-10-07 22:11:11 +0530

    crack

---
+++ src/com/rohan/String/RemoveSpaceFromString.java
@@ -0,0 +1,14 @@
+package com.rohan.String;
+
+public class RemoveSpaceFromString {
+    public static void main(String[] args) {
+        String name = "Java is Programming Language";
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < name.length(); i++) {
+            if(name.charAt(i) != ' '){
+                sb.append(name.charAt(i));
+            }
+        }
+        System.out.println(sb.toString());
+    }
+}
