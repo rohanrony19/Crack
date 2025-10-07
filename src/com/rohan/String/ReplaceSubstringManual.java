c34cc80dad1301460756c768cfeffa1a0d6b1c80
Rohan <rohanprasad1434@gmail.com>
2025-10-07 22:11:18 +0530

    crack

---
+++ src/com/rohan/String/ReplaceSubstringManual.java
@@ -0,0 +1,20 @@
+package com.rohan.String;
+
+public class ReplaceSubstringManual {
+    public static void main(String[] args) {
+        String str = "Hello World";
+        String target = "World";
+        String replacement = "Java";
+
+        String res = "";
+        for (int i = 0; i < str.length(); i++) {
+            if (i <= str.length() - target.length() && str.substring(i,i+target.length()).equals(target)){
+                res = res + replacement;
+                i = i + target.length()-1;
+            }else {
+                res =  res + str.charAt(i);
+            }
+        }
+        System.out.println(res);
+    }
+}
