d82afce8bb6b0910a081d2af64d25b32706b6bb5
Rohan <rohanprasad1434@gmail.com>
2025-10-07 22:10:27 +0530

    crack

---
+++ src/com/rohan/String/Palindrome.java
@@ -0,0 +1,17 @@
+package com.rohan.String;
+// Time Complexity = O(n^2)
+// Space Complexity = O(n)
+public class Palindrome {
+    public static void main(String[] args) {
+        String name = "madam";
+        String rev = "";
+        for (int i = name.length()-1; i >=0 ; i--) {
+            rev += name.charAt(i);
+        }
+        if(name.equals(rev)){
+            System.out.println(name + " is Palindrome");
+        }else{
+            System.out.println(name + " is not a Palindrome");
+        }
+    }
+}
