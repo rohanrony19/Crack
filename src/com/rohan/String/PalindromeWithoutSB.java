7c629e50e95b597873a206fc21ad31fd3749650b
Rohan <rohanprasad1434@gmail.com>
2025-10-07 22:10:40 +0530

    crack

---
+++ src/com/rohan/String/PalindromeWithoutSB.java
@@ -0,0 +1,21 @@
+package com.rohan.String;
+// Time Complexity = O(n)
+// Space Complexity = O(1)
+public class PalindromeWithoutSB {
+    public static void main(String[] args) {
+        String name = "madam";
+        boolean isPalindrome = true;
+
+        int i = 0;
+        int j = name.length() - 1;
+        while (i < j){
+            if (name.charAt(i)!=name.charAt(j)){
+                isPalindrome = false;
+                break;
+            }
+            i++;
+            j--;
+        }
+        System.out.println(name + (isPalindrome ? " is Palindrome" : " is not a Palindrome"));
+    }
+}
