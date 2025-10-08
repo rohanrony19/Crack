519c8aec4a65c21d968c2019a4aac9770a414459
Rohan <rohanprasad1434@gmail.com>
2025-10-08 23:15:45 +0530

    crack

--- src/com/rohan/Algorithm/Binary.java
+++ src/com/rohan/Algorithm/Binary.java
@@ -1,23 +1,22 @@
 package com.rohan.Algorithm;
-
+// Time Complexity = O(log n)
+// Space Complexity = O(1)
 public class Binary {
 
     public static int search(int [] arr,int key){
         int start=0;
         int end=arr.length-1;
         while(start<=end){
-            int mid=(start+end)/2;
+            int mid=start+(end-start)/2;
             if(arr[mid]==key){
                 return mid;
             }
             else if(key>arr[mid]){
                   start=mid+1;
-
-
             }
             else{
                   end=mid-1;
-                             }
+            }
         }
         return -1;
     }
@@ -25,7 +24,7 @@ public class Binary {
 
     public static void main(String[] args) {
         int [] arr={10,20,30,40,50,60};  // index 5 , length 6
-        int key =40;
+        int key =400;
         int res=search(arr,key);
         System.out.println(res);
     }}
