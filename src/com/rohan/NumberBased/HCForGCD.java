73a1f18358804a285f81c317911d8e02b09edd5e
Rohan <rohanprasad1434@gmail.com>
2025-10-09 23:50:18 +0530

    crack

---
+++ src/com/rohan/NumberBased/HCForGCD.java
@@ -0,0 +1,23 @@
+package com.rohan.NumberBased;
+// Highest common Factor both a and b which are divisible by highest number
+// The largest number that divides both numbers.  ->  GCD(12, 18) = 6
+// Time Complexity = O(log(min(a,b)))
+// Space Complexity = O(n)
+public class HCForGCD {
+    public static int gcd(int a,int b){
+        while (b != 0){
+            int temp = b;
+            b = a % b;
+          //b = 24 % 36 = 24<36 = 24
+          //b = 36 % 24 = 12 remainder
+          //b = 24 % 12 = 0 remainder
+            a = temp;
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        int a = 24,b = 36;
+        System.out.println("GCD = " + gcd(a,b));
+    }
+}
