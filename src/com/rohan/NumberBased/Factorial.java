0e7925db45b8466e43833b9a62122d5e2c02d1a9
Rohan <rohanprasad1434@gmail.com>
2025-10-09 23:49:47 +0530

    crack

---
+++ src/com/rohan/NumberBased/Factorial.java
@@ -0,0 +1,22 @@
+package com.rohan.NumberBased;
+
+import java.util.Scanner;
+// Time Complexity = O(n)
+// Space Complexity = O(1)
+public class Factorial {
+    public static long fact(int n){
+        long fact = 1;
+        for (int i = 2; i <= n; i++) {
+                fact = fact * i ;
+            }
+        return fact;
+        }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number to get Factorial : ");
+        int n = sc.nextInt();
+        System.out.println("Factorial of " + n + " is " + fact(n));
+    }
+    }
+
