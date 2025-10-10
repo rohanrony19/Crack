3d43fe047a33a2708b42404e448e1a813c3783b0
Rohan <rohanprasad1434@gmail.com>
2025-10-10 23:17:35 +0530

    crack

---
+++ src/com/rohan/NumberBased/Palindrome.java
@@ -0,0 +1,19 @@
+package com.rohan.NumberBased;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        String name = "madam";
+        boolean isPalindrome = true;
+        int start = 0;
+        int end = name.length()-1;
+        while (start < end){
+            if(name.charAt(start) != name.charAt(end)){
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+        System.out.println(name + (isPalindrome?" is Palindrome" : " is not a Palindrome"));
+    }
+}
