b0edde62627193af29c26ef25bc65150b6c6a3f8
Rohan <rohanprasad1434@gmail.com>
2025-10-10 23:17:49 +0530

    crack

---
+++ src/com/rohan/NumberBased/PrimeNumber.java
@@ -0,0 +1,21 @@
+package com.rohan.NumberBased;
+// Brute Force method
+// Time Complexity = O(n)
+// Space Complexity = O(1)
+public class PrimeNumber {
+    static boolean prime(int n){
+        if(n<=1) return false;
+
+        for (int i = 2; i < n; i++) {
+            if(n % i == 0 ){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int num = 375113;
+        System.out.println(num + (prime(num)? " is Prime":" is not a Prime"));
+    }
+}
