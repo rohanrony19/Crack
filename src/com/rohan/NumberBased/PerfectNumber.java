443a051d257bff331c4b0b4f9e27b071ca48f27a
Rohan <rohanprasad1434@gmail.com>
2025-10-10 23:17:39 +0530

    crack

---
+++ src/com/rohan/NumberBased/PerfectNumber.java
@@ -0,0 +1,17 @@
+package com.rohan.NumberBased;
+// Time Complexity = O(n)
+// Space Complexity = O(1)
+public class PerfectNumber {
+    public static boolean isPerfect(int n){
+        int sum = 0;
+        for (int i = 1; i <= n/2; i++) {
+            if (n % i == 0) sum += i;
+        }
+        return sum == n;
+    }
+
+    public static void main(String[] args) {
+        int n = 28;
+        System.out.println(n + " is Perfect? " + isPerfect(n));
+    }
+}
