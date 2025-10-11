3321ae94ede409449dd6701d4157e9bdb126a2a2
Rohan <rohanprasad1434@gmail.com>
2025-10-11 23:44:11 +0530

    crack

---
+++ src/com/rohan/NumberBased/SumOfDigits.java
@@ -0,0 +1,17 @@
+package com.rohan.NumberBased;
+
+public class SumOfDigits {
+    public static int sumDigits(int n){
+        int sum = 0;
+        while(n>0){
+            sum = sum + n%10;
+            n = n/10;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int n = 1234;
+        System.out.println("Sum of Digits : " + sumDigits(n));
+    }
+}
