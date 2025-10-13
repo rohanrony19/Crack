31bd49f47cf7895c18438fa9f7f63e726489431f
Rohan <rohanprasad1434@gmail.com>
2025-10-13 22:12:35 +0530

    crack

---
+++ src/com/rohan/Collections/Map/ImmutableMapDemo.java
@@ -0,0 +1,17 @@
+package com.rohan.Collections.Map;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ImmutableMapDemo {
+    public static void main(String[] args) {
+        Map<String,Integer> map1 = new HashMap<>();
+        map1.put("A",1);
+        map1.put("B",2);
+        Map<String,Integer> map2 = Collections.unmodifiableMap(map1);
+        System.out.println(map2);
+        map2.put("C",3); // throws exception bcz its immutable
+
+    }
+}
