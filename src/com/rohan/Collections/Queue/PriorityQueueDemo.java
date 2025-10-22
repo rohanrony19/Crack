a2540b81b3dcf59014312850a741758205243821
Rohan <rohanprasad1434@gmail.com>
2025-10-22 23:30:00 +0530

    crack

--- src/com/rohan/Collections/Queue/PriorityQueueDemo.java
+++ src/com/rohan/Collections/Queue/PriorityQueueDemo.java
@@ -17,7 +17,6 @@ public class PriorityQueueDemo {
         System.out.println(pq.peek());  //20
 
 
-
         while (!pq.isEmpty()){
             System.out.println(pq.poll()); // 20,25,30
         }
