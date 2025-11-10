2fdec9fb53b6fb0b00230705ec0ed887aac5e8a0
Rohan <rohanprasad1434@gmail.com>
2025-11-10 21:40:13 +0530

    crack

---
+++ src/com/rohan/String/DivideTwoInteger.java
@@ -0,0 +1,17 @@
+package com.rohan.String;
+
+public class DivideTwoInteger {
+    public static int divide(int dividend, int divisor) {
+        if (dividend == Integer.MIN_VALUE && divisor == -1) {
+            return Integer.MAX_VALUE;
+        }
+        return dividend/divisor;
+    }
+
+    public static void main(String[] args) {
+        int dividend = 10;
+        int divisor = 3;
+        int result = divide(dividend,divisor);
+        System.out.println(result);
+    }
+}
