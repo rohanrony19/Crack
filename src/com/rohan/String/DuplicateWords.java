f06005474091d562933d81b447a60e7b6ee95e2c
Rohan <rohanprasad1434@gmail.com>
2025-11-12 21:13:04 +0530

    crack

--- src/com/rohan/String/DuplicateWords.java
+++ src/com/rohan/String/DuplicateWords.java
@@ -1,11 +1,13 @@
 package com.rohan.String;
 
+import java.util.HashMap;
+
 public class DuplicateWords {
     public static void main(String[] args) {
         String sentence = "this is is a test test";
         String[] words = sentence.split(" ");
 
-        java.util.HashMap<String, Integer> map = new java.util.HashMap<>();
+        HashMap<String, Integer> map = new HashMap<>();
         for (String w : words) {
             map.put(w, map.getOrDefault(w, 0) + 1);
         }
