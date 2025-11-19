79a9ad8631e6c1c599fd7e6363a3956fd5016c79
Rohan <rohanprasad1434@gmail.com>
2025-11-19 23:24:44 +0530

    leetcode

---
+++ src/com/rohan/leetcode/easy/RomanToInteger.java
@@ -0,0 +1,47 @@
+package com.rohan.leetcode.easy;
+
+//Symbol       Value
+//I             1
+//V             5
+//X             10
+//L             50
+//C             100
+//D             500
+//M             1000
+
+//Example 2:
+//Input: s = "LVIII"
+//Output: 58
+//Explanation: L = 50, V= 5, III = 3.
+
+//Example 3:
+//Input: s = "MCMXCIV"
+//Output: 1994
+//Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+
+//s=O(1),t=O(n)
+public class RomanToInteger {
+    public static int romanToInt(String s){
+        int ans = 0, num = 0;
+        for (int i = s.length()-1; i >= 0 ; i--) {
+            switch (s.charAt(i)){
+                case 'I' : num = 1;break;
+                case 'V': num = 5; break;
+                case 'X': num = 10; break;
+                case 'L': num = 50; break;
+                case 'C': num = 100; break;
+                case 'D': num = 500; break;
+                case 'M': num = 1000; break;
+            }
+            if (4 * num < ans) ans -= num;
+            else ans += num;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        String s = "MCMXCIV";
+        int res = romanToInt(s);
+        System.out.println(res);
+    }
+}
