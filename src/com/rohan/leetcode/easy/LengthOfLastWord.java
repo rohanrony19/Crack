39038a9780a293e6806d800f2009a4bf7b9d8292
Rohan <rohanprasad1434@gmail.com>
2025-11-22 13:14:10 +0530

    leetcode

---
+++ src/com/rohan/leetcode/easy/LengthOfLastWord.java
@@ -0,0 +1,26 @@
+package com.rohan.leetcode.easy;
+
+//Example 1:
+//Input: s = "Hello World"
+//Output: 5
+//Explanation: The last word is "World" with length 5.
+
+//Example 2:
+//Input: s = "   fly me   to   the moon  "
+//Output: 4
+//Explanation: The last word is "moon" with length 4.
+
+//Example 3:
+//Input: s = "luffy is still joyboy"
+//Output: 6
+//Explanation: The last word is "joyboy" with length 6.
+
+public class LengthOfLastWord {
+    public int lengthOfLastWord(String s){
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
