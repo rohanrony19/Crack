bbc1a34819a010b68c8a530d5a9e6e0fa6bb012b
Rohan <rohanprasad1434@gmail.com>
2025-11-22 13:13:42 +0530

    leetcode

--- src/com/rohan/leetcode/easy/AddBinary.java
+++ src/com/rohan/leetcode/easy/AddBinary.java
@@ -1,13 +1,14 @@
 package com.rohan.leetcode.easy;
 
 //Example 1:
-//
 //Input: a = "11", b = "1"
 //Output: "100"
+
 //Example 2:
-//
 //Input: a = "1010", b = "1011"
 //Output: "10101"
+
+// t,s = O(max(m,n))
 
 public class AddBinary {
     public static String addBinary(String a,String b){
