ca2b1a64767c93fe1841f67bb49bc50ceeb6694a
Rohan <rohanprasad1434@gmail.com>
2025-11-22 13:09:39 +0530

    leetcode

--- src/com/rohan/leetcode/easy/AddBinary.java
+++ src/com/rohan/leetcode/easy/AddBinary.java
@@ -15,7 +15,17 @@ public class AddBinary {
         int i = a.length()-1;
         int j = b.length()-1;
         int carry = 0;
-        
+        while (i >= 0 || j >= 0 || carry != 0){
+            int sum = carry;
+            if(i >= 0){
+                sum += a.charAt(i) - '0';
+                i--;
+            }
+            if(j >= 0){
+                sum += b.charAt(j) - '0';
+                j--;
+            }
+        }
     }
 
     public static void main(String[] args) {
