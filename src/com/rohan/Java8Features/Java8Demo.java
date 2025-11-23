857c750bffbd9ee34d80faf6dbeeee4cab7198b2
Rohan N Prasad <149262357+rohanrony19@users.noreply.github.com>
2025-11-23 22:50:14 +0530

    Java8

--- src/com/rohan/Java8Features/Java8Demo.java
+++ src/com/rohan/Java8Features/Java8Demo.java
@@ -4,6 +4,6 @@ public class Java8Demo {
     public static void main(String[] args) {
         ArrayList<String> arr = Arrays.asList(1,2,3,4,5,6);
         stream.filter(x->x%2).count();
-            
+
     }
 }
