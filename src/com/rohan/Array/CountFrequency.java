8e20835e7f86a1ddb7e6b4fd06766e4d3290a46f
Rohan <rohanprasad1434@gmail.com>
2025-11-25 21:56:27 +0530

    array

---
+++ src/com/rohan/Array/CountFrequency.java
@@ -0,0 +1,15 @@
+package com.rohan.Array;
+
+public class CountFrequency {
+    public static void main(String[] args) {
+        int[] arr = {2,5,2,6,2,7};
+        int target = 2;
+        int count = 0;
+        for (int num:arr){
+            if(num == target){
+                count++;
+            }
+        }
+        System.out.println("Frequency : " + count);
+    }
+}
