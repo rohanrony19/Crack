f94c1cf3aebb3ab90d63d3aee18f1a63ee6c1129
Rohan <rohanprasad1434@gmail.com>
2025-11-25 21:56:22 +0530

    array

--- src/com/rohan/Array/CheckSorted.java
+++ src/com/rohan/Array/CheckSorted.java
@@ -6,8 +6,8 @@ public class CheckSorted {
         int[] arr = {5,3,4,1,2};
         boolean isSorted = true;
 
-        for (int i = 0; i < arr.length-1; i++) {
-            if(arr[i]>arr[i+1]){
+        for (int i = 1; i < arr.length; i++) {
+            if(arr[i] < arr[i-1]){
                 isSorted = false;
                 break;
             }
