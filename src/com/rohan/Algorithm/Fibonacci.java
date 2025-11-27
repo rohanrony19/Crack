534975f1c0984d796b82ade692fadcacd0634656
Rohan <rohanprasad1434@gmail.com>
2025-11-27 22:47:59 +0530

    algorithm

--- src/com/rohan/Algorithm/Fibonacci.java
+++ src/com/rohan/Algorithm/Fibonacci.java
@@ -1,4 +1,15 @@
 package com.rohan.Algorithm;
 
 public class Fibonacci {
+    public static void main(String[] args) {
+        int n = 15;
+        int a = 0;
+        int b = 1;
+        for (int i = 2; i <= n ; i++) {
+            int c = a + b;
+            a=b;
+            b=c;
+        }
+        System.out.println(a);
+    }
 }
