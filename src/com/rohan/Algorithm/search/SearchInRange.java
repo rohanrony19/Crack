47b80a782973aeedb70dde39c0d4ae8cffee4d17
Rohan <rohanprasad1434@gmail.com>
2025-11-27 22:48:28 +0530

    algorithm

---
+++ src/com/rohan/Algorithm/search/SearchInRange.java
@@ -0,0 +1,20 @@
+package com.rohan.Algorithm.search;
+
+public class SearchInRange {
+    public static void main(String[] args) {
+        int[] arr = {2,4,5,7,22,19,4};
+        int target = 19;
+        int start = 2;
+        int end = 6;
+        System.out.println(linearSearch(arr,target,start,end));
+    }
+    public static int linearSearch(int[] arr, int target,int start,int end){
+        if(arr.length==0)return -1;
+        for (int index = start; index <= end; index++) {
+            if(arr[index] == target){
+                return index;
+            }
+        }
+        return -1;
+    }
+}
