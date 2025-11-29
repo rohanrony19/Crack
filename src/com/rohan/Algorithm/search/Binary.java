efb302c12695b6833a44179205012124642e6bae
Rohan <rohanprasad1434@gmail.com>
2025-11-29 23:56:02 +0530

    algorithm

--- src/com/rohan/Algorithm/search/Binary.java
+++ src/com/rohan/Algorithm/search/Binary.java
@@ -1,20 +1,22 @@
 package com.rohan.Algorithm.search;
 // Time Complexity = O(log n)
 // Space Complexity = O(1)
+// Divide and Conquer
 public class Binary {
     public static int search(int [] arr,int target){
         int start=0;
         int end=arr.length-1;
         while(start<=end){
+//            int mid = (start+end) / 2;  possibility that it may exceed int range
             int mid=start+(end-start)/2;
-            if(arr[mid]==target){
-                return mid;
+            if(target < arr[mid]){
+                end = mid -1;
             }
             else if(target>arr[mid]){
-                  start=mid+1;
+                start = mid + 1;
             }
             else{
-                  end=mid-1;
+                  return mid;
             }
         }
         return -1;
@@ -23,7 +25,7 @@ public class Binary {
 
     public static void main(String[] args) {
         int [] arr={10,20,30,40,50,60};  // index 5 , length 6
-        int target =400;
+        int target =40;
         int res=search(arr,target);
         System.out.println(res);
     }
