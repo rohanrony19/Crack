4d3fb146c6734f9c6650e92ac65f00888988190f
Rohan <rohanprasad1434@gmail.com>
2025-12-01 23:14:31 +0530

    leetcode

---
+++ src/com/rohan/leetcode/easy/FindDisappearedNumber.java
@@ -0,0 +1,50 @@
+package com.rohan.leetcode.easy;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+// 448
+// Google
+/*
+Example 1:
+Input: nums = [4,3,2,7,8,2,3,1]
+Output: [5,6]
+
+Example 2:
+Input: nums = [1,1]
+Output: [2]
+ */
+
+public class FindDisappearedNumber {
+    public static void main(String[] args) {
+        int[] nums = {4,3,2,7,8,2,3,1};
+        List<Integer> res = findDisappearedNumbers(nums);
+        System.out.println(res);
+    }
+
+    public static List<Integer> findDisappearedNumbers(int[] nums) {
+        int i = 0;
+        while (i < nums.length){
+            int correct = nums[i] - 1;
+            if(nums[i] != nums[correct]){
+                swap(nums,i,correct);
+            } else {
+                i++;
+            }
+        }
+        // just find missing numbers
+        List<Integer> ans = new ArrayList<>();
+        for (int index = 0; index < nums.length; index++) {
+            if(nums[index] != index+1){
+                ans.add(index + 1);
+            }
+        }
+        return ans;
+    }
+
+    static void swap(int[] arr,int first,int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
