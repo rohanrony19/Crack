615d278b8894b225bb0fda919a6dbd96a5a9fa2f
Rohan <rohanprasad1434@gmail.com>
2025-12-03 21:59:22 +0530

    pattern

---
+++ src/com/rohan/Patterns/Pattern11.java
@@ -0,0 +1,28 @@
+package com.rohan.Patterns;
+/*
+0 0 0 0 0 0 0 0 0
+0 1 1 1 1 1 1 1 0
+0 1 2 2 2 2 2 1 0
+0 1 2 3 3 3 2 1 0
+0 1 2 3 4 3 2 1 0
+0 1 2 3 3 3 2 1 0
+0 1 2 2 2 2 2 1 0
+0 1 1 1 1 1 1 1 0
+0 0 0 0 0 0 0 0 0
+ */
+public class Pattern11 {
+    public static void main(String[] args) {
+        p11(4);
+    }
+
+    static void p11(int n){
+        n = 2 * n;
+        for (int row = 1; row <= n; row++) {
+            for (int col = 0; col <= n; col++) {
+                int atEveryIndex = Math.min(Math.min(row,col),Math.min(n - row,n - col));
+                System.out.print(atEveryIndex + " ");
+            }
+            System.out.println();
+        }
+    }
+}
