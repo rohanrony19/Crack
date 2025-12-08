fdfcc5f95fb117f01c455ac7ca3c7a212f667106
Rohan <rohanprasad1434@gmail.com>
2025-12-08 23:52:19 +0530

    LL

--- src/com/rohan/Collections/List/linkedlist/LL.java
+++ src/com/rohan/Collections/List/linkedlist/LL.java
@@ -110,7 +110,7 @@ public class LL {
     }
 
     // insert using recursion
-    public void insertRec(int val,int inde)
+    public void insertRec(int val,int index)
 
     public void display(){
         Node temp = head;
