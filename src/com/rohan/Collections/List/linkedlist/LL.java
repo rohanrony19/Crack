195aeec035206524cb18bcabb2115c5ded15fa40
Rohan <rohanprasad1434@gmail.com>
2025-12-09 23:16:25 +0530

    LL

--- src/com/rohan/Collections/List/linkedlist/LL.java
+++ src/com/rohan/Collections/List/linkedlist/LL.java
@@ -111,7 +111,17 @@ public class LL {
 
     // insert using recursion
     public void insertRec(int val,int index){
+        head = insertRec(val,index,head);
+    }
+    private Node insertRec(int val,int index, Node node){
+        if(index == 0){
+            Node temp = new Node(val,node);
+            size++;
+            return temp;
+        }
 
+        node.next = insertRec(val, index-1,node.next);
+        return node;
     }
 
     public void display(){
@@ -135,5 +145,75 @@ public class LL {
             this.value = value;
             this.next = next;
         }
+    }
+
+    // Questions
+    public void duplicates(){
+        Node node = head;
+
+        while (node.next != null){
+            if(node.value == node.next.value){
+                node.next = node.next.next;
+                size--;
+            }else {
+                node = node.next;
+            }
+        }
+        tail = node;
+        tail.next = null;
+    }
+
+    // merge
+    public static LL merge(LL first,LL second){
+        Node f = first.head;
+        Node s = second.head;
+
+        LL ans = new LL();
+
+        while (f != null && s != null){
+            if(f.value < s.value){
+                ans.insertLast(f.value);
+                f = f.next;
+            }else {
+                ans.insertLast(s.value);
+                s = s.next;
+            }
+        }
+        while (f != null){
+            ans.insertLast(f.value);
+            f = f.next;
+        }
+        while (s != null){
+            ans.insertLast(s.value);
+            s = s.next;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+//        LL list = new LL();
+//        list.insertLast(1);
+//        list.insertLast(1);
+//        list.insertLast(2);
+//        list.insertLast(3);
+//        list.insertLast(3);
+//        list.insertLast(3);
+//        list.display();
+//        list.duplicates();
+//        list.display();
+
+        LL first = new LL();
+        first.insertLast(1);
+        first.insertLast(3);
+        first.insertLast(5);
+
+        LL second = new LL();
+        second.insertLast(1);
+        second.insertLast(2);
+        second.insertLast(9);
+        second.insertLast(14);
+
+        LL ans = LL.merge(first,second);
+        ans.display();
     }
 }
