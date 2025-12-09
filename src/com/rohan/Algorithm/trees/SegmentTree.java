5b1270df1f59e6e388b7308330b5dac457fd10db
Rohan <rohanprasad1434@gmail.com>
2025-12-10 04:52:58 +0530

    tree

--- src/com/rohan/Algorithm/trees/SegmentTree.java
+++ src/com/rohan/Algorithm/trees/SegmentTree.java
@@ -5,7 +5,7 @@ public class SegmentTree {
         int[] arr = {3,8,6,7,-2,-8,4,9};
 
         SegmentTree tree = new SegmentTree(arr);
-//        tree.display();
+        tree.display();
         System.out.println(tree.query(1,6));
     }
 
