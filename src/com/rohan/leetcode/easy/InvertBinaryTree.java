c4bcf45489b9b1649f21268e3f4038ae4a411258
Rohan <rohanprasad1434@gmail.com>
2025-12-14 00:47:02 +0530

    leetcode

---
+++ src/com/rohan/leetcode/easy/InvertBinaryTree.java
@@ -0,0 +1,73 @@
+package com.rohan.leetcode.easy;
+// 226
+/*
+Example 1:
+Input: root = [4,2,7,1,3,6,9]
+Output: [4,7,2,9,6,3,1]
+
+Example 2:
+Input: root = [2,1,3]
+Output: [2,3,1]
+
+Example 3:
+Input: root = []
+Output: []
+ */
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class InvertBinaryTree {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null){
+            return null;
+        }
+
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+
+        root.left = right;
+        root.right = left;
+
+        return root;
+    }
+
+    public static void main(String[] args) {
+        InvertBinaryTree tree = new InvertBinaryTree();
+        TreeNode node = new TreeNode(4);
+        node.left = new TreeNode(2);
+        node.right = new TreeNode(7);
+        node.left.left = new TreeNode(1);
+        node.left.right = new TreeNode(3);
+        node.right.left = new TreeNode(6);
+        node.right.right = new TreeNode(9);
+
+        tree.invertTree(node);
+
+        // Level order print
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(node);
+        while(!q.isEmpty()) {
+            int size = q.size();
+            for(int i=0; i<size; i++) {
+                TreeNode n = q.poll();
+                System.out.print(n.val + " ");
+                if(n.left != null) q.add(n.left);
+                if(n.right != null) q.add(n.right);
+            }
+            System.out.println();
+        }
+    }
+}
