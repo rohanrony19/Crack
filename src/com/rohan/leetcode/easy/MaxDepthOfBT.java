1d6a8f3f4e7f266641a934f2b59c830280e08045
Rohan <rohanprasad1434@gmail.com>
2025-12-14 00:47:21 +0530

    leetcode

---
+++ src/com/rohan/leetcode/easy/MaxDepthOfBT.java
@@ -0,0 +1,51 @@
+package com.rohan.leetcode.easy;
+// 104
+// Amazon
+/*
+Example 1:
+Input: root = [3,9,20,null,null,15,7]
+Output: 3
+
+Example 2:
+Input: root = [1,null,2]
+Output: 2
+ */
+
+public class MaxDepthOfBT {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static int maxDepth(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+
+        int depth = Math.max(left,right) + 1;
+
+        return depth;
+    }
+
+
+    public static void main(String[] args) {
+        MaxDepthOfBT depth = new MaxDepthOfBT();
+        TreeNode node = new TreeNode(3);
+        node.left = new TreeNode(9);
+        node.right = new TreeNode(20);
+        node.right.left = new TreeNode(15);
+        node.right.right = new TreeNode(7);
+        System.out.println(depth.maxDepth(node));
+    }
+}
