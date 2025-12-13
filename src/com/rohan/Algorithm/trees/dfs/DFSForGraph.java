dec6f2d65f194f1a76f91852f61a5661218710d6
Rohan <rohanprasad1434@gmail.com>
2025-12-14 00:48:00 +0530

    Algorithm

---
+++ src/com/rohan/Algorithm/trees/dfs/DFSForGraph.java
@@ -0,0 +1,37 @@
+package com.rohan.Algorithm.trees.dfs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DFSForGraph {
+    static void dfs(int node, boolean[] visited, List<List<Integer>> graph) {
+        visited[node] = true;
+        System.out.print(node + " ");
+
+        for (int neighbor : graph.get(node)) {
+            if (!visited[neighbor]) {
+                dfs(neighbor, visited, graph);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int n = 5; // number of nodes
+        List<List<Integer>> graph = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        // Build Graph
+        graph.get(0).add(1);
+        graph.get(0).add(2);
+        graph.get(1).add(3);
+        graph.get(2).add(4);
+
+        boolean[] visited = new boolean[n];
+
+        dfs(0, visited, graph);
+    }
+}
