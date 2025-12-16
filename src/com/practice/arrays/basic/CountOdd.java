33cdf114fc445f57c92fee50090dec5d731bf6fa
Rohan <rohanprasad1434@gmail.com>
2025-12-16 23:16:37 +0530

    practice

---
+++ src/com/practice/arrays/basic/CountOdd.java
@@ -0,0 +1,15 @@
+package com.practice.arrays.basic;
+
+public class CountOdd {
+    public static void main(String[] args) {
+        int[] arr = {1, 4, 6, 7, 9, 10};
+        int count = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 != 0){
+                count++;
+            }
+        }
+        System.out.println("Odd count: " + count);
+    }
+}
