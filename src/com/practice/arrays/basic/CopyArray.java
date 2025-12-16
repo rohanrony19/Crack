711953d3088b0fa30116a8b52fa33f457fe25ad8
Rohan <rohanprasad1434@gmail.com>
2025-12-16 23:16:09 +0530

    practice

---
+++ src/com/practice/arrays/basic/CopyArray.java
@@ -0,0 +1,15 @@
+package com.practice.arrays.basic;
+
+import java.util.Arrays;
+
+public class CopyArray {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40};
+        int[] copy = new int[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            copy[i] = arr[i];
+        }
+        System.out.println(Arrays.toString(copy));
+    }
+}
