cc2cb071e3ece8de61c21bc2ae859c081b8f0f7d
Rohan <rohanprasad1434@gmail.com>
2025-12-16 23:17:14 +0530

    practice

---
+++ src/com/practice/arrays/intermediate/DuplicatesBrute.java
@@ -0,0 +1,16 @@
+package com.practice.arrays.intermediate;
+
+public class DuplicatesBrute {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 2, 4, 1};
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i+1; j < arr.length; j++) {
+                if(arr[i] == arr[j]){
+                    System.out.print(arr[i] + " ");
+                    break;
+                }
+            }
+        }
+    }
+}
