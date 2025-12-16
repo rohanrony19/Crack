42ec7e06d2d357bed1f02ae0890a5e24bf6ad884
Rohan <rohanprasad1434@gmail.com>
2025-12-16 23:16:05 +0530

    practice

---
+++ src/com/practice/strings/basic/ContainsDigit.java
@@ -0,0 +1,16 @@
+package com.practice.strings.basic;
+
+public class ContainsDigit {
+    public static void main(String[] args) {
+        String s = "abc123";
+
+        boolean found = false;
+        for (char c:s.toCharArray()){
+            if(Character.isDigit(c)){
+                found = false;
+                break;
+            }
+        }
+        System.out.println(found);
+    }
+}
