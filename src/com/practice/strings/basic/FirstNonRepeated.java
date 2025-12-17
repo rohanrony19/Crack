763f8f73794a759daf1bf2b938df05afff9b4e46
Rohan <rohanprasad1434@gmail.com>
2025-12-17 23:50:32 +0530

    practice

---
+++ src/com/practice/strings/basic/FirstNonRepeated.java
@@ -0,0 +1,17 @@
+package com.practice.strings.basic;
+
+public class FirstNonRepeated {
+    public static void main(String[] args) {
+        String s = "swiss";
+        int[] freq = new int[256];
+
+        for (char c:s.toCharArray()) freq[c]++;
+
+        for (char c:s.toCharArray()){
+            if(freq[c] == 1){
+                System.out.println(c);
+                break;
+            }
+        }
+    }
+}
