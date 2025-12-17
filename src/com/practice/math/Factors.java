38ca7d1aaeb1e0331366ff94d32b7d8abdc5bdc1
Rohan <rohanprasad1434@gmail.com>
2025-12-17 23:50:09 +0530

    practice

---
+++ src/com/practice/math/Factors.java
@@ -0,0 +1,13 @@
+package com.practice.math;
+
+public class Factors {
+    public static void main(String[] args) {
+        int n = 28;
+
+        for (int i = 1; i <= n; i++) {
+            if(n % i == 0){
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
