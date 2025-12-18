8d41fbebd8f5eb4d21c3204d2155ed5fd93b8c28
Rohan <rohanprasad1434@gmail.com>
2025-12-18 23:52:35 +0530

    practice

---
+++ src/com/practice/recursion/basic/FactorialRec.java
@@ -0,0 +1,12 @@
+package com.practice.recursion.basic;
+
+public class FactorialRec {
+    static int fact(int n){
+        if(n <= 1) return 1;
+        return n * fact(n-1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fact(5));
+    }
+}
