54c3e655f68873e596ddf6aac191002a152abfd1
Rohan <rohanprasad1434@gmail.com>
2025-12-18 23:53:02 +0530

    practice

---
+++ src/com/practice/arrays/intermediate/InsertElementIndex.java
@@ -0,0 +1,23 @@
+package com.practice.arrays.intermediate;
+
+import java.util.Arrays;
+
+public class InsertElementIndex {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40};
+        int index = 2;
+        int value = 99;
+
+        int[] newArr = new int[arr.length + 1];
+
+        for (int i = 0; i < index; i++) {
+            newArr[i] = arr[i];
+        }
+        newArr[index] = value;
+
+        for (int i = index; i < arr.length; i++) {
+            newArr[i + 1] = arr[i];
+        }
+        System.out.println(Arrays.toString(newArr));
+    }
+}
