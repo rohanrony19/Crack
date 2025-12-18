171086ffe29fb28116d69f9c71712204c9727977
Rohan <rohanprasad1434@gmail.com>
2025-12-18 23:53:31 +0530

    practice

---
+++ src/com/practice/strings/basic/LowerToUpper.java
@@ -0,0 +1,8 @@
+package com.practice.strings.basic;
+
+public class LowerToUpper {
+    public static void main(String[] args) {
+        String s = "hello";
+        System.out.println(s.toUpperCase());
+    }
+}
