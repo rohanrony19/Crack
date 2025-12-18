bf718e4f2802bcf33f47a151c992d099430d7aa3
Rohan <rohanprasad1434@gmail.com>
2025-12-18 23:52:53 +0530

    practice

---
+++ src/com/practice/basic/Hello.java
@@ -0,0 +1,10 @@
+package com.practice.basic;
+
+public class Hello {
+    public static void main(String[] args) {
+        String name ="Hello";
+        for (int i = 0; i < name.length(); i++) {
+            System.out.println(name);
+        }
+    }
+}
