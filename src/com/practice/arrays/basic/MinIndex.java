2c0902a4c0e18168e3d15fe702ab29e904f38f92
Rohan <rohanprasad1434@gmail.com>
2025-12-19 23:19:46 +0530

    practice

---
+++ src/com/practice/arrays/basic/MinIndex.java
@@ -0,0 +1,17 @@
+package com.practice.arrays.basic;
+
+public class MinIndex {
+    public static void main(String[] args) {
+        int[] arr = {10,50,20,40};
+        int min = arr[0];
+        int index = 0;
+
+        for (int i = 1; i < arr.length; i++) {
+            if(arr[i] < min){
+                min = arr[i];
+                index = i;
+            }
+        }
+        System.out.println("Index: " + index);
+    }
+}
