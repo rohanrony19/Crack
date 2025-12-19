bbb5453776f7ec7e946806b4194fe500f0ec76a1
Rohan <rohanprasad1434@gmail.com>
2025-12-19 23:19:14 +0530

    practice

---
+++ src/com/practice/arrays/sorting/basic/DescSort.java
@@ -0,0 +1,14 @@
+package com.practice.arrays.sorting.basic;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class DescSort {
+    public static void main(String[] args) {
+        Integer[] arr = {10,40,20,5};
+
+        Arrays.sort(arr, Collections.reverseOrder());
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
