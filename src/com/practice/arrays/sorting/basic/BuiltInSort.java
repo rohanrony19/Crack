79b66e220b5b0487ea479040ab7fb8cef1b546d5
Rohan <rohanprasad1434@gmail.com>
2025-12-20 22:54:49 +0530

    practice

---
+++ src/com/practice/arrays/sorting/basic/BuiltInSort.java
@@ -0,0 +1,11 @@
+package com.practice.arrays.sorting.basic;
+
+import java.util.Arrays;
+
+public class BuiltInSort {
+    public static void main(String[] args) {
+        int[] arr = {5,4,3,2,1};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
