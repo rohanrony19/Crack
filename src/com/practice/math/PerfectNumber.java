4ab3d86d4b59226f068b7132566bdd185d6a95ed
Rohan <rohanprasad1434@gmail.com>
2025-12-20 22:55:30 +0530

    practice

---
+++ src/com/practice/math/PerfectNumber.java
@@ -0,0 +1,15 @@
+package com.practice.math;
+/*
+Perfect = sum of proper divisors (excluding number itself).
+Example: 6 → 1 + 2 + 3 = 6
+ */
+public class PerfectNumber {
+    public static void main(String[] args) {
+        int n = 6,sum = 0;
+
+        for (int i = 1; i <= n/2; i++) {
+            if(n % i == 0) sum += i;
+        }
+        System.out.println(sum == n ? "Perfect" : "Not Perfect");
+    }
+}
