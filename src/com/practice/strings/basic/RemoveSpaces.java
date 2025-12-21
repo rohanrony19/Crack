5bceb628dc1d97a2ec31803c8a92751c02f7d9dd
Rohan <rohanprasad1434@gmail.com>
2025-12-21 23:56:14 +0530

    practice

---
+++ src/com/practice/strings/basic/RemoveSpaces.java
@@ -0,0 +1,9 @@
+package com.practice.strings.basic;
+
+public class RemoveSpaces {
+    public static void main(String[] args) {
+        String s = "a b c d e";
+        s = s.replace(" ","");
+        System.out.println(s);
+    }
+}
