347bf4f8acb36adc3ebbf86169f4e8a5050696b9
Rohan <rohanprasad1434@gmail.com>
2025-12-21 23:55:59 +0530

    practice

---
+++ src/com/practice/arrays/intermediate/RemoveDuplicatesSorted.java
@@ -0,0 +1,16 @@
+package com.practice.arrays.intermediate;
+
+public class RemoveDuplicatesSorted {
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 2, 2, 3, 4, 4};
+        int j = 0;
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != arr[j]){
+                j++;
+                arr[j] = arr[i];
+            }
+        }
+        System.out.println("New Length = " + (j + 1));
+    }
+}
