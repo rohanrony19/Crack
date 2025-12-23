2ef090d16785ee5e6e897c44b7b64e71db6a411c
Rohan <rohanprasad1434@gmail.com>
2025-12-23 23:32:49 +0500

    dsa

--- src/com/rony/dsa/arrays/sorting/AllInOne.java
+++ src/com/rony/dsa/arrays/sorting/AllInOne.java
@@ -4,15 +4,6 @@ import java.util.Arrays;
 
 public class AllInOne {
     public static void main(String[] args) {
-        int[] arr = {1,3,5,2,4};
-        int target = 2;
-        int res = linear(arr,target);
-        System.out.println(res);
-
-        int[] arr1 = {1,2,3,4,5,8};
-        int target1 = 4;
-        System.out.println(binary(arr1,target1));
-
         int[] arr2 = {8,5,1,2,3,4};
         int[] res1 = bubble(arr2);
         System.out.println(Arrays.toString(res1));
@@ -28,32 +19,6 @@ public class AllInOne {
         int[] arr4 = {8,5,1,2,3,4};
         int[] res3 = insertion(arr4);
         System.out.println(Arrays.toString(res3));
-    }
-
-    public static int linear(int[] arr,int target){
-        //{1,3,5,2,4} , 2
-        for (int i = 0; i < arr.length; i++) {
-            if(arr[i] == target){
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    public static int binary(int[] arr,int target){
-        //{1,3,5,2,4} , 2
-        int start = 0,end = arr.length-1;
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            if(arr[mid] < target){
-                start = mid + 1;
-            }else if(arr[mid] > target){
-                end = mid - 1;
-            }else {
-                return mid;
-            }
-        }
-        return -1;
     }
 
     public static int[] bubble(int[] arr){
