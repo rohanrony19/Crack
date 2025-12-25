c532c62e1b6291a69a8ffeca489392644d3f8212
Rohan <rohanprasad1434@gmail.com>
2025-12-25 23:26:14 +0430

    dsa

---
+++ src/com/rony/dsa/arrays/searching/SearchRotated.java
@@ -0,0 +1,31 @@
+package com.rony.dsa.arrays.searching;
+
+public class SearchRotated {
+    public static int search(int[] arr, int target) {
+        int l = 0, r = arr.length - 1;
+
+        while(l <= r) {
+            int mid = (l + r) / 2;
+
+            if(arr[mid] == target) return mid;
+
+            if(arr[l] <= arr[mid]) { // left sorted
+                if(target >= arr[l] && target < arr[mid])
+                    r = mid - 1;
+                else
+                    l = mid + 1;
+            } else { // right sorted
+                if(target > arr[mid] && target <= arr[r])
+                    l = mid + 1;
+                else
+                    r = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4,5,6,7,0,1,2};
+        System.out.println(search(arr, 0));
+    }
+}
