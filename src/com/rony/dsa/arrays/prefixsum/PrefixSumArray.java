49830d04fe5f7e6fd24303966889fa25309df2f8
Rohan <rohanprasad1434@gmail.com>
2025-12-28 23:01:01 +0530

    dsa

--- src/com/rony/dsa/arrays/prefixsum/PrefixSumArray.java
+++ src/com/rony/dsa/arrays/prefixsum/PrefixSumArray.java
@@ -15,7 +15,7 @@ public class PrefixSumArray {
         int[] prefix = new int[arr.length];
 
         prefix[0] = arr[0];
-        
+
         for (int i = 1; i < arr.length; i++) {
             prefix[i] = prefix[i-1] + arr[i];
         }
