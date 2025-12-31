ab594e165ecaa70e32171220479240657065343e
Rohan <rohanprasad1434@gmail.com>
2025-12-31 18:23:04 +0530

    dsa

---
+++ src/com/rony/rohan/Collections/problems/comparablevscomparator/Employee.java
@@ -0,0 +1,20 @@
+package com.rony.rohan.Collections.problems.comparablevscomparator;
+
+import java.util.Comparator;
+
+public class Employee{
+    int id;
+    String name;
+    double salary;
+
+    public Employee(int id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return id + " " + name + " " + salary;
+    }
+}
