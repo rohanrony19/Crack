acc00e22d0cb9857854e68f801e40604b18bf105
Rohan <rohanprasad1434@gmail.com>
2026-01-05 23:33:16 +0530

    dsa

---
+++ src/com/rony/rohan/Collections/problems/ll/MiddleOfLL.java
@@ -0,0 +1,19 @@
+package com.rony.rohan.Collections.problems.ll;
+
+public class MiddleOfLL {
+    static class Person {
+        String name;
+        Person friend;
+    }
+
+    public static void main(String[] args) {
+        Person p1 = new Person();
+        p1.name = "Rohan";
+
+
+    }
+
+    static Person middle(Person head){
+
+    }
+}
