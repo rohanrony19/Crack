03d7b93517806d56775266ea1dbe3aabc5c0b261
Rohan <rohanprasad1434@gmail.com>
2026-01-22 21:58:27 +0530

    dsa

--- src/com/rony/dsa/arrays/basic/FindMax.java
+++ src/com/rony/dsa/arrays/basic/FindMax.java
@@ -5,7 +5,7 @@ public class FindMax {
         int[] arr = {5,8,20,1,44,6};
         int max = arr[0];
 
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 1; i < arr.length; i++) {
             if (arr[i] > max){
                 max = arr[i];
             }
