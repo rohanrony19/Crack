8e0eb1330bc92d533d47e98aa4a300853b575874
Rohan <rohanprasad1434@gmail.com>
2026-01-22 22:04:20 +0530

    dsa

--- src/com/rony/dsa/arrays/intermediate/FirstNonRepeating.java
+++ src/com/rony/dsa/arrays/intermediate/FirstNonRepeating.java
@@ -1,7 +1,22 @@
 package com.rony.dsa.arrays.intermediate;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class FirstNonRepeating {
     public static void main(String[] args) {
+        int[] arr = {4,5,4,6,5,7};
 
+        Map<Integer,Integer> freq = new HashMap<>();
+        for (int num:arr){
+            freq.put(num,freq.getOrDefault(num,0) + 1);
+        }
+
+        for (int n:arr){
+            if(freq.get(n) == 1){
+                System.out.println(n);
+                break;
+            }
+        }
     }
 }
