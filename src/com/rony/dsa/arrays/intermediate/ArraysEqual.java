93f917dec42a79c8a7b22f145251188de613637c
Rohan <rohanprasad1434@gmail.com>
2026-01-23 22:03:48 +0530

    dsa

--- src/com/rony/dsa/arrays/intermediate/ArraysEqual.java
+++ src/com/rony/dsa/arrays/intermediate/ArraysEqual.java
@@ -4,17 +4,26 @@ import java.util.Arrays;
 
 public class ArraysEqual {
     public static void main(String[] args) {
-        int[] a = {1,2,3};
-        int[] b = {1,2,3};
+        int[] a = {1, 2, 3};
+        int[] b = {1, 2, 3, 4};
 
-        System.out.println(Arrays.equals(a,b));
-        // or
+        // standard
+        System.out.println("Arrays.equal: " + Arrays.equals(a, b));
+
+        //manual
+        System.out.println("Manual check: " + areArrayEqual(a,b));
+
+    }
+
+    public static boolean areArrayEqual(int[] a,int[] b){
+        if(a.length != b.length){
+           return false;
+        }
         for (int i = 0; i < a.length; i++) {
-            if(a[i] != b[i]){
-                System.out.println("not match");
-                break;
+            if (a[i] != b[i]) {
+                return false;
             }
         }
-        System.out.println("match");
+        return true;
     }
 }
