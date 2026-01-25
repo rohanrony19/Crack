0893e85b9554717fd2b1fb352a76f8878c0400b9
Rohan <rohanprasad1434@gmail.com>
2026-01-25 23:26:21 +0530

    dsa

--- src/com/rony/dsa/arrays/intermediate/CountPairs.java
+++ src/com/rony/dsa/arrays/intermediate/CountPairs.java
@@ -5,7 +5,6 @@ public class CountPairs {
         int[] arr = {1,5,7,-1,5};
         int target = 6;
         int count = 0;
-        
 
         for (int i = 0; i < arr.length; i++) {
             for (int j = i+1; j < arr.length; j++) {
