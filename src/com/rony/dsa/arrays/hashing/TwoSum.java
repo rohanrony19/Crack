b3bc516160094e0c711a507edf28cf55f57c635c
Rohan <rohanprasad1434@gmail.com>
2026-01-26 22:46:25 +0530

    dsa

---
+++ src/com/rony/dsa/arrays/hashing/TwoSum.java
@@ -0,0 +1,24 @@
+package com.rony.dsa.arrays.hashing;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// print first pair
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] arr = {2,7,11,15,-4};
+        int target = 9;
+
+        Map<Integer,Integer> map = new HashMap<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            int need = target - arr[i];
+
+            if (map.containsKey(need)){
+                System.out.println("Indices: " + map.get(need) + ", " + i );
+                return;
+            }
+            map.put(arr[i],i);
+        }
+    }
+}
