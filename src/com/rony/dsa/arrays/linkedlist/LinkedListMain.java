18af7ddce4285ff292881945a60348898eb794ec
Rohan <rohanprasad1434@gmail.com>
2026-01-29 23:04:08 +0530

    dsa

--- src/com/rony/dsa/arrays/linkedlist/LinkedListMain.java
+++ src/com/rony/dsa/arrays/linkedlist/LinkedListMain.java
@@ -2,13 +2,22 @@ package com.rony.dsa.arrays.linkedlist;
 
 public class LinkedListMain {
     public static void main(String[] args) {
-        
+
         LinkedList ll = new LinkedList();
         ll.addLast(10);
         ll.addLast(20);
         ll.addLast(30);
         ll.addLast(40);
         ll.addLast(50);
+        ll.addLast(60);
+        ll.addLast(70);
+        ll.addLast(80);
+
+        ll.print();
+
+        ll.findMiddle();
+
+        ll.head = ll.reverse();
 
         ll.print();
 
