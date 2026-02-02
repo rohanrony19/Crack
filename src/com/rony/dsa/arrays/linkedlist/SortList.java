1c009f86d6d1190db34b17ec7217a3bffdfc14b8
Rohan <rohanprasad1434@gmail.com>
2026-02-02 21:58:53 +0530

    dsa

---
+++ src/com/rony/dsa/arrays/linkedlist/SortList.java
@@ -0,0 +1,25 @@
+package com.rony.dsa.arrays.linkedlist;
+
+public class SortList {
+    public static void main(String[] args) {
+
+    }
+
+    public Node getMid(Node head){
+        Node slow = head;
+        Node fast = head.next;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    Node mergeSort(Node head){
+        if(head == null || head.next == null){
+            return head;
+        }
+
+    }
+}
