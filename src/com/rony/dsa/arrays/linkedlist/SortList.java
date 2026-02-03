7adb77024d91e7d5096b315d3a600b3b0abb96bb
Rohan <rohanprasad1434@gmail.com>
2026-02-03 23:26:15 +0530

    dsa

--- src/com/rony/dsa/arrays/linkedlist/SortList.java
+++ src/com/rony/dsa/arrays/linkedlist/SortList.java
@@ -1,8 +1,35 @@
 package com.rony.dsa.arrays.linkedlist;
 
 public class SortList {
+    Node head;
+    Node tail;
     public static void main(String[] args) {
+        SortList ll = new SortList();
+        ll.addLast(40);
+        ll.addLast(80);
+        ll.addLast(50);
+        ll.addLast(10);
+        ll.addLast(90);
+        ll.addLast(20);
+        ll.addLast(60);
+        ll.addLast(30);
 
+        ll.print();
+
+        ll.head = ll.mergeSort(ll.head);
+
+        ll.print();
+    }
+
+    public void addLast(int data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = newNode;
+            tail = head;
+        }else{
+            tail.next = newNode;
+            tail = newNode;
+        }
     }
 
     Node mergeSort(Node head){
@@ -18,7 +45,7 @@ public class SortList {
 
         return merge(left,right);
     }
-    
+
     public Node getMid(Node head){
         Node slow = head;
         Node fast = head.next;
@@ -30,5 +57,25 @@ public class SortList {
         return slow;
     }
 
+    Node merge(Node a,Node b){
+        if(a == null) return b;
+        if (b == null) return a;
 
+        if(a.data <= b.data){
+            a.next = merge(a.next,b);
+            return a;
+        }else {
+            b.next = merge(a,b.next);
+            return b;
+        }
+    }
+
+    public void print(){
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
 }
