f9ebc7f6199fb7ed5d25f25110b003fcca0993bf
Rohan <rohanprasad1434@gmail.com>
2026-02-12 23:34:51 +0530

    dsa

--- src/com/rony/dsa/arrays/sorting/CyclicSort.java
+++ src/com/rony/dsa/arrays/sorting/CyclicSort.java
@@ -14,11 +14,11 @@ public class CyclicSort {
 
         int i =0;
         while(i<arr.length){
-            int correct = arr[i] - 1;
-            if(arr[i] != arr[correct]){
+            int index = arr[i] - 1;
+            if(arr[i] != arr[index]){
                 int temp = arr[i];
-                arr[i] = arr[correct];
-                arr[correct] = temp;
+                arr[i] = arr[index];
+                arr[index] = temp;
             }else{
                 i++;
             }
