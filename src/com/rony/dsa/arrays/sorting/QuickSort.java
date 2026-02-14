9e63cf9ee215d50a32ca132800a3a86b56855729
Rohan <rohanprasad1434@gmail.com>
2026-02-14 22:51:51 +0530

    dsa

---
+++ src/com/rony/dsa/arrays/sorting/QuickSort.java
@@ -0,0 +1,40 @@
+package com.rony.dsa.arrays.sorting;
+
+import java.util.Arrays;
+
+public class QuickSort {
+    public static void main(String[] args) {
+        int[] arr = {4,1,7,2,5,3,6};
+        sort(arr,0,arr.length-1);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void sort(int[] arr, int low, int high) {
+        if(low >= high){
+            return;
+        }
+
+        int start = low;
+        int end = high;
+        int mid = start + (end-start)/2;
+        int pivot = arr[mid];
+
+        while(start <= end){
+            while (arr[start] < pivot){
+                start++;
+            }
+            while (arr[end] > pivot){
+                end--;
+            }
+            if(start <= end){
+                int temp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp;
+                start++;
+                end--;
+            }
+        }
+        sort(arr,low,end);
+        sort(arr,start,high);
+    }
+}
