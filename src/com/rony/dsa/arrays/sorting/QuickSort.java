432e325ece5b94260e2981f9dc4588d0f0177539
Rohan <rohanprasad1434@gmail.com>
2026-02-15 23:09:40 +0530

    dsa

--- src/com/rony/dsa/arrays/sorting/QuickSort.java
+++ src/com/rony/dsa/arrays/sorting/QuickSort.java
@@ -12,7 +12,6 @@ public class QuickSort {
     private static void sort(int[] arr, int low, int high) {
         if(low >= high){
             return;
-            
         }
 
         int start = low;
