5f4a4afce76c8f46fb0352f0d0230ad8d523526d
Rohan <rohanprasad1434@gmail.com>
2026-03-07 21:47:58 +0530

    dsa

--- src/com/rony/dsa/arrays/linkedlist/LinkedListMain.java
+++ src/com/rony/dsa/arrays/linkedlist/LinkedListMain.java
@@ -6,6 +6,7 @@ public class LinkedListMain {
         LinkedList ll = new LinkedList();
         ll.addLast(10);
         ll.addLast(20);
+        
         ll.addLast(30);
         ll.addLast(40);
         ll.addLast(50);
