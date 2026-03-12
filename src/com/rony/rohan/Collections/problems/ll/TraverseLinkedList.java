c525af840ba11f7a158681752a816190cff0ef67
Rohan <rohanprasad1434@gmail.com>
2026-03-12 21:53:27 +0530

    dsa

--- src/com/rony/rohan/Collections/problems/ll/TraverseLinkedList.java
+++ src/com/rony/rohan/Collections/problems/ll/TraverseLinkedList.java
@@ -13,6 +13,7 @@ public class TraverseLinkedList {
 
     public static void main(String[] args) {
         Node head = new Node(10);
+
         head.next = new Node(20);
         head.next.next = new Node(30);
         head.next.next.next = new Node(40);
