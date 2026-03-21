5afd56e48167d15322bc761e5a46a6ca33d0a9da
Rohan <rohanprasad1434@gmail.com>
2026-03-21 21:36:24 +0530

    dsa

--- src/com/rony/rohan/leetcode/easy/DiameterOfBinaryTree.java
+++ src/com/rony/rohan/leetcode/easy/DiameterOfBinaryTree.java
@@ -21,8 +21,8 @@ public class DiameterOfBinaryTree {
         TreeNode(int val, TreeNode left, TreeNode right) {
             this.val = val;
             this.left = left;
-            this.right = right; 
-        }
+            this.right = right;
+        } 
     }
     int diameter = 0;
 
