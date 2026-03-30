4e8b61ea67188c82970cbeba12a55ef0aecbc14b
Rohan <rohanprasad1434@gmail.com>
2026-03-30 22:41:47 +0530

    dsa

---
+++ src/com/rony/dsa/arrays/hmm/CheckSubsetOfOther.java
@@ -0,0 +1,35 @@
+package com.rony.dsa.arrays.hmm;
+
+import java.util.Arrays;
+
+public class CheckSubsetOfOther {
+    public static void main(String[] args) {
+        int[] a = {11, 1, 13, 21, 3, 7};
+        int[] b = {11, 3, 7, 1};
+
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        int i = 0, j = 0;
+        int m = a.length, n = b.length;
+
+        while (i < m && j < n) {
+            if (a[i] < b[j]) {
+                // move in a to catch up
+                i++;
+            }
+            else if (a[i] == b[j]) {
+                i++;
+                // matched one element from b
+                j++;
+            }
+            else {
+                // a[i] > b[j] → means b[j] is missing
+                System.out.println(-1);
+            }
+        }
+
+        // all b[] matched
+        System.out.println(j==n);
+    }
+}
