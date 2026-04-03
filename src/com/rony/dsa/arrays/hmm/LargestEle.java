85ec7455a91217f93f678c2a0244f4b98c5a788d
Rohan <rohanprasad1434@gmail.com>
2026-04-03 23:14:40 +0530

    dsa

---
+++ src/com/rony/dsa/arrays/hmm/LargestEle.java
@@ -0,0 +1,15 @@
+package com.rony.dsa.arrays.hmm;
+
+public class LargestEle {
+    public static void main(String[] args) {
+        int[] arr = {10,20,3,40,12,25};
+        int max = arr[0];
+
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] > max){
+                max = arr[i];
+            }
+        }
+        System.out.println("Maximum: " + max);
+    }
+}
