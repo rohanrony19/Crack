091e5f6aafb83e6b04fa8ce58f10c8a5c03854cc
Rohan <rohanprasad1434@gmail.com>
2026-04-05 23:14:19 +0530

    dsa

---
+++ src/com/rony/dsa/arrays/hmm/RemoveCharacter.java
@@ -0,0 +1,19 @@
+package com.rony.dsa.arrays.hmm;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class RemoveCharacter {
+    public static void main(String[] args) {
+        String s1 = "computer";
+        String s2 = "cat";
+
+        for (int i = 0; i < s2.length(); i++) {
+            char c = s2.charAt(i);
+            s1 = s1.replace((c + ""),"");
+
+        }
+        System.out.println(s1);
+    }
+}
