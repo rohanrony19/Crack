c3676e935b496265537c6a4417159606476729c7
Rohan <rohanprasad1434@gmail.com>
2026-04-10 23:31:33 +0530

    dsa

---
+++ src/com/rony/dsa/arrays/hmm/SumArray.java
@@ -0,0 +1,13 @@
+package com.rony.dsa.arrays.hmm;
+
+public class SumArray {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        int sum = 0;
+
+        for (int num:arr){
+            sum += num;
+        }
+        System.out.println(sum);
+    }
+}
