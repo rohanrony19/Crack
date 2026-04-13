6f84c430da66659faa3d342f07986d4f44be2e25
Rohan <rohanprasad1434@gmail.com>
2026-04-13 21:31:42 +0530

    dsa

--- src/com/rony/dsa/math/GCD.java
+++ src/com/rony/dsa/math/GCD.java
@@ -8,7 +8,6 @@ package com.rony.dsa.math;
 public class GCD {
     public static int gcd(int a,int b){
         if(b == 0) return a;
-        
         return gcd(b,a%b);
     }
     public static void main(String[] args) {
