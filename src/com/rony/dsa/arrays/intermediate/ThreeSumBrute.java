299f07e978a4176261795ee30ec82be17fb081ab
Rohan <rohanprasad1434@gmail.com>
2026-04-14 21:25:39 +0530

    dsa

--- src/com/rony/dsa/arrays/intermediate/ThreeSumBrute.java
+++ src/com/rony/dsa/arrays/intermediate/ThreeSumBrute.java
@@ -7,7 +7,6 @@ public class ThreeSumBrute {
         for (int i = 0; i < arr.length; i++) {
             for (int j = i+1; j < arr.length; j++) {
                 for (int k = j+1; k < arr.length; k++) {
-                    
                     if(arr[i] + arr[j] + arr[k] == 0){
                         System.out.println(arr[i] + ", " + arr[j] + ", " + arr[k]);
                     }
