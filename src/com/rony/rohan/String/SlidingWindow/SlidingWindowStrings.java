01c68d547e6f2235b78fc1768a5e7633dd28af1e
Rohan <rohanprasad1434@gmail.com>
2026-04-26 21:16:46 +0530

    dsa

--- src/com/rony/rohan/String/SlidingWindow/SlidingWindowStrings.java
+++ src/com/rony/rohan/String/SlidingWindow/SlidingWindowStrings.java
@@ -11,6 +11,7 @@ public class SlidingWindowStrings {
         int sum = 0;
 
         for (int i = 0; i < k; i++) sum += (int) s.charAt(i);
+
         int maxSum = sum;
 
         for (int i = k; i < n; i++) {
