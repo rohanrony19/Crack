316b0e50a6a3e65c18da7456f61f2dc336e7c015
Rohan <rohanprasad1434@gmail.com>
2026-08-02 20:49:27 +0530

    dsa

--- src/com/rony/rohan/Array/LinearSearch.java
+++ src/com/rony/rohan/Array/LinearSearch.java
@@ -6,6 +6,7 @@ public class LinearSearch {
         int[] arr = {4, 1, 2, 5, 3};
         int target = 5;
         boolean isfound = false;
+
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == target) {
                 System.out.println("Found at index: " + i);
