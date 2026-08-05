a4620b4941ccbc1f28bd793fe170a665fdce5e5d
Rohan <rohanprasad1434@gmail.com>
2026-08-05 23:23:30 +0530

    dsa

--- src/com/rony/rohan/Array/Kadane.java
+++ src/com/rony/rohan/Array/Kadane.java
@@ -8,6 +8,7 @@ public class Kadane {
         int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
         int maxSoFar = arr[0];
         int curr = arr[0];
+
         for (int i = 1; i < arr.length; i++) {
             curr = Math.max(arr[i],curr + arr[i]);
             maxSoFar = Math.max(maxSoFar,curr);
